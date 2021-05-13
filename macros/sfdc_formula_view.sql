2b05511d6c1a82e3bfd3a800bfa22cf768df8d53
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-05-13 15:07:07 -0500

    exclude old fields add

--- macros/sfdc_formula_view.sql
+++ macros/sfdc_formula_view.sql
@@ -1,13 +1,15 @@
 {%- macro sfdc_formula_view(join_to_table_first) -%}
 
-
 {{
     config(
         materialized = 'view'
     )
 }}
+
+{%- set old_formula_fields = dbt_utils.get_column_values(source('salesforce', 'fivetran_formula'),'field') -%}
+    
     select 
-        *,
+        {{ dbt_utils.star(source('salesforce',join_to_table_first), except=old_formula_fields) }},
         {{ salesforce_formula_utils.sfdc_formula_pivot(join_to_table=join_to_table_first) }}
     from {{ source('salesforce',join_to_table_first) }}
 
