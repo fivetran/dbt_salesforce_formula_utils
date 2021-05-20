a25e79e8e792d037f22aed214ba768bbb2117f5c
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-05-20 16:16:50 -0500

    inline comments addition

--- macros/sfdc_formula_pivot.sql
+++ macros/sfdc_formula_pivot.sql
@@ -1,10 +1,12 @@
 {%- macro sfdc_formula_pivot(join_to_table) -%}
 
+    -- The results generated from sfdc_formula_refactor are set to the key_val variable.
     {%- set key_val = salesforce_formula_utils.sfdc_formula_refactor(join_to_table) -%}
 
+    -- k being the field name and v being the sql.
     {% for k, v in key_val %}
         {% if v == 'null_value' %}
-            null as {{ k }}
+            null as {{ k }} -- Switching the string null to a true null as we will no longer need to iterate through a nonetype.
         {% else %}
             {{ v }} as {{ k }}
         {% endif %}
