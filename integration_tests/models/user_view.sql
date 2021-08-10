a0c89a3a38fc76e12aa3a0ae59429ebfa20182a9
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-08-10 17:14:47 -0500

    integration tests macro adjustment

--- integration_tests/models/user_view.sql
+++ integration_tests/models/user_view.sql
@@ -1,1 +1,1 @@
-{{ salesforce_formula_utils.sfdc_formula_view(join_to_table_first='user',reserved_table_name='user_table') }}
+{{ salesforce_formula_utils.sfdc_formula_view(source_table='user',reserved_table_name='user_table') }}
