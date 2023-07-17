86cc8efa9987a6bc0de03fb0bbd62e8e6c568d71
fivetran-reneeli <renee.li@fivetran.com>
2023-07-17 11:19:52 -0400

    add databricks to backtick conditional

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -19,4 +19,6 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
+dbt run --vars '{full_statement_version: false}' --target "$db" 
+dbt test --target "$db"
 dbt run-operation fivetran_utils.drop_schemas_automation --target "$db"
--- macros/sfdc_formula_view_sql.sql
+++ macros/sfdc_formula_view_sql.sql
@@ -12,14 +12,14 @@
             --The select statement must explicitly query from and join from the source, not the target. The replace filters point the query to the source.
             {% if ' from ' in v %}
                 {%- set v = v | replace(' from ',' from ' + source(source_name,'fivetran_formula') | string ) -%}
-                {% if target.type == 'bigquery' %} {%- set v = v | replace('`fivetran_formula`','') -%} 
+                {% if target.type == 'bigquery', 'spark', 'databricks' %} {%- set v = v | replace('`fivetran_formula`','') -%} 
                 {% elif target.type == 'redshift' %} {%- set v = v | replace('"fivetran_formula"', '') -%} 
                 {% else %} {%- set v = v | replace('fivetran_formula','') -%} {% endif %}
             {% endif %}
 
             {% if ' left join ' in v %}
                 {%- set v = v | replace(' left join ',' left join ' + source(source_name,'fivetran_formula') | string ) -%}
-                {% if target.type == 'bigquery' %} {%- set v = v | replace('`fivetran_formula`','') -%} 
+                {% if target.type == 'bigquery', 'spark', 'databricks' %} {%- set v = v | replace('`fivetran_formula`','') -%} 
                 {% elif target.type == 'redshift' %} {%- set v = v | replace('"fivetran_formula"', '') -%} 
                 {% else %} {%- set v = v | replace('fivetran_formula','') -%} {% endif %}
             {% endif %}
