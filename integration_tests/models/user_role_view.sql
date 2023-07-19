b9d31121689a29a4dd8baaf3967e334599823221
fivetran-reneeli <renee.li@fivetran.com>
2023-07-19 15:09:16 -0400

    fix typo

--- integration_tests/models/user_role_view.sql
+++ integration_tests/models/user_role_view.sql
@@ -1,3 +1,3 @@
-{{ salesforce_formula_utils.sfdc_formula_viewsfdc_formula_view(
+{{ salesforce_formula_utils.sfdc_formula_view(
     source_table = 'user_role',
     full_statement_version = false) }}
