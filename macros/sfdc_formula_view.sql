9578cb3ac60a51d32b5975d972193a9b116d1a61
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-07-01 18:56:48 -0500

    revisions from review

--- macros/sfdc_formula_view.sql
+++ macros/sfdc_formula_view.sql
@@ -1,4 +1,4 @@
-{%- macro sfdc_formula_view(source_table, source_name='salesforce', reserved_table_name=source_table, fields_to_include=none, materialization='view', using_quoted_identifiers=False, full_statement_version=true) -%}
+{%- macro sfdc_formula_view(source_table, source_name='salesforce', materialization='view', using_quoted_identifiers=False, full_statement_version=true) -%}
 
 -- Best practice for this model is to be materialized as view. That is why we have set that here.
 {{
@@ -7,16 +7,11 @@
     )
 }}
 
--- Raise a compiler error if users are trying to use full_statement_version=false. We are keeping the variable in the macro, however, since we don't want errors if they previously set it to true.
+-- Raise a warning if users are trying to use full_statement_version=false. We are keeping the variable in the macro, however, since we don't want errors if they previously set it to true.
 {% if not full_statement_version %}
-{{ exceptions.raise_compiler_error("\ERROR: Parameter full_statement_version=false is no longer supported. Update your " ~ this.identifier|upper ~ " model to remove the full_statement_version parameter.\n") }}
+{{ exceptions.warn("\ERROR: Parameters full_statement_version=false, reserved_table_name, and fields_to_include are no longer supported. Update your " ~ this.identifier|upper ~ " model to remove these parameters.\n") }}
+* Refer to error in log.
 {% endif %}
-
-/*
-    The below sets the old_formula_fields variable to the results of the get_column_values results which queries the field column from the fivetran_formula table.
-    The logic here is that the variable will be a list of all current salesforce formula field names. This list is then used within the dbt_utils.star operation to exclude them.
-    This allows users with the Fivetran legacy Salesforce fields to ignore them and be replaced by the new fields.
-*/
 
 {% if using_quoted_identifiers %}
 {%- set table_results = dbt_utils.get_column_values(table=source(source_name, 'fivetran_formula_model'), 
